9ad365215925dc26f8fcb40a689d9140128198d5
Student <student@Mac.local>
2024-01-23 08:46:44 -0800

    Adding Add Master Pro and Hello World

---
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace Calculator;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		Title = "Add Master Pro";
+	}
+
+    void Add_Clicked(System.Object sender, System.EventArgs e)
+    {
+        double firstNum;
+        double secondNum;
+        
+        Double.TryParse(txtFirstNum.Text, out firstNum);
+        Double.TryParse(txtSecondNum.Text, out secondNum);
+        lblDisplay.Text = (firstNum + secondNum).ToString();
+        
+
+    }
+
+    void Clear_Clicked(System.Object sender, System.EventArgs e)
+    {
+        txtFirstNum.Text = string.Empty;
+        txtSecondNum.Text = string.Empty;
+        lblDisplay.Text = "0";
+        txtFirstNum.Focus();
+    }
+}
+
+
