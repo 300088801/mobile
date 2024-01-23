5e4f246492a7a42bcc711fc7b004e02671034504
Student <student@Mac.local>
2024-01-23 09:47:01 -0800

    Commit of roman numeral converter

---
+++ Roman/Roman/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace Roman;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void Convert_Clicked(System.Object sender, System.EventArgs e)
+    {
+        Int32 input;
+        bool isNumber;
+        Converter converter = new Converter();
+
+        isNumber = Int32.TryParse(txtInput.Text, out input);
+
+        if(isNumber)
+        {
+            lblDisplay.Text = converter.NumberToRoman(input);
+        }
+        else
+        {
+            lblDisplay.Text = converter.RomanToNumber(txtInput.Text).ToString();
+        }
+    }
+
+    void Clear_Clicked_1(System.Object sender, System.EventArgs e)
+    {
+        txtInput.Text = string.Empty;
+        lblDisplay.Text = "0";
+    }
+}
+
+
