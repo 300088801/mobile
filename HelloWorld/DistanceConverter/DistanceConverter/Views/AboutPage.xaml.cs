9834ee52ae684f1ad5cd6b8c25f43b64d9030eed
Student <student@Mac.local>
2024-02-27 07:15:13 -0800

    Save Stuff app about saving stuff to the config file

--- HelloWorld/DistanceConverter/DistanceConverter/Views/AboutPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DistanceConverter.Views;
-
-public partial class AboutPage : ContentPage
-{
-	public AboutPage()
-	{
-		InitializeComponent();
-	}
-}
---
+++ SaveStuff/SaveStuff/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace SaveStuff;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void Save_Clicked(System.Object sender, System.EventArgs e)
+    {
+        Preferences.Set("UserName", txtUserName.Text);
+        //saving stuff to the config file
+    }
+
+    void Read_Clicked(System.Object sender, System.EventArgs e)
+    {
+        txtUserName.Text = Preferences.Get("UserName", "");
+    }
+
+    void Clear_Clicked(System.Object sender, System.EventArgs e)
+    {
+        txtUserName.Text = string.Empty;
+    }
+}
+
+
